88adbe5fcbe5e655bf2a3b661dbd90a4bdcc76e7
arobbins <arobbins@mitre.org>
2024-03-27 16:06:29 -0400

    make fixes to remove prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,15 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
+  root: true,
   parserOptions: {
-		parser: '@typescript-eslint/parser',
-		ecmaVersion: 2020
-	},
-  "extends": [
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+  },
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier",
     "@vue/typescript",
-		"@vue/typescript/recommended",
-    "@vue/prettier/@typescript-eslint"
-  ]
-}
+    "@vue/typescript/recommended",
+  ],
+};
