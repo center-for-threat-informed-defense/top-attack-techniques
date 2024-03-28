a695b98fea3fcc469b435f535aadf0e24d9cbc63
arobbins <arobbins@mitre.org>
2024-03-28 14:22:37 -0400

    feat(TAT-138): overall page structure

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
-    <div>This is the Top ATT&CK Techniques calculator page</div>
-    <div>Data from store: {{ exampleStore.name }}</div>
-  </div>
+  <section class="w-5/6 mx-auto py-20 text-center">
+    <h1 class="uppercase text-4xl">Top ATT&CK Techniques Calculator</h1>
+    <p>Use the following criteria to score your system and click generate in order to see your personalized list of techniques</p>
+    <div class="container-row">
+      <div class="lg:w-2/5 container">
+        <div class="container-header">
+          <h2>Filters</h2>
+        </div>
+        <div class="container-body">
+
+        </div>
+      </div>
+      <div class="lg:w-3/5 container">
+        <div class="container-header">
+          <h2>Score your system</h2>
+        </div>
+        <div class="container-body">
+
+        </div>
+      </div>
+    </div>
+    <button class="btn-primary w-1/3">Generate</button>
+  </section>
 </template>
 
 <script lang="ts">
