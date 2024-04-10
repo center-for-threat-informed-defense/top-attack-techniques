f53029a006fcf90d5722ef11633db6beee527afb
arobbins <arobbins@mitre.org>
2024-04-10 17:05:55 -0400

    refactor(TAT-123): replace & with @amp;

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,14 +4,14 @@
       <div class="w-5/6 h-full mx-auto my-auto">
         <div class="title">
           <h1 class="">
-            <span class="text-ctid-light-purple">Top</span> ATT&CK Techniques
+            <span class="text-ctid-light-purple">Top</span> ATT&amp;CK Techniques
           </h1>
           <div class="lg:w-6 lg:h-36 w-full h-1 bg-ctid-light-purple my-4"></div>
           <p class="my-auto">
-            Top ATT&CK Techniques provides defenders with a systematic approach
-            to prioritizing ATT&CK techniques. Our open methodology considers
+            Top ATT&amp;CK Techniques provides defenders with a systematic approach
+            to prioritizing ATT&amp;CK techniques. Our open methodology considers
             technique prevalence, common attack choke points, and actionability
-            to enable defenders to focus on the ATT&CK techniques that are most
+            to enable defenders to focus on the ATT&amp;CK techniques that are most
             relevant to their organization.
           </p>
         </div>
@@ -20,13 +20,13 @@
             description="Use the criteria to score your system and click generate in order to see your personalized list of techniques. Our calculator will show you a customized list of the vulnerabilities you have and what you need to prioritize for your system."
             :imgSrc="CalculatorSvg" link="calculator" />
           <section-item title="Top 10 lists"
-            description="The Top Ransomware Technique List provides a starting point for defending against ransomware attacks and demonstrates how the Top ATT&CK Techniques methodology can be tailored to different use cases."
+            description="The Top Ransomware Technique List provides a starting point for defending against ransomware attacks and demonstrates how the Top ATT&amp;CK Techniques methodology can be tailored to different use cases."
             :imgSrc="ListSvg" link="top-10-lists" />
           <section-item title="Methodology"
             description="Use the criteria to score your system and click generate in order to see your personalized list of techniques. Our calculator will show you a customized list of the vulnerabilities you have and what you need to prioritize for your system."
             :imgSrc="BookSvg" link="methodology" />
           <section-item title="Help"
-            description="The Top Ransomware Technique List provides a starting point for defending against ransomware attacks and demonstrates how the Top ATT&CK Techniques methodology can be tailored to different use cases."
+            description="The Top Ransomware Technique List provides a starting point for defending against ransomware attacks and demonstrates how the Top ATT&amp;CK Techniques methodology can be tailored to different use cases."
             :imgSrc="HelpSvg" link="help" />
         </div>
       </div>
@@ -38,7 +38,7 @@
       <div class="md:flex my-10 lg:w-2/3 w-5/6 mx-auto lg:gap-40 md: gap-10">
         <div class="md:w-1/2">
           <p>
-            The methodology page outlines the rationale behind the ATT&CK
+            The methodology page outlines the rationale behind the ATT&amp;CK
             technique scores and ranking. A technique’s score is calculated by
             the three core components: Actionability, Choke Point, and
             Prevalence.
@@ -50,8 +50,8 @@
         <div class="md:w-1/2">
           <p>
             The help page contains the answers to common questions and issues
-            that may pop up for users of Top ATT&CK Techniques. It also has a
-            step by step guide to learning your top ATT&CK techniques customized
+            that may pop up for users of Top ATT&amp;CK Techniques. It also has a
+            step by step guide to learning your top ATT&amp;CK techniques customized
             to your system.
           </p>
           <router-link to="/help">
