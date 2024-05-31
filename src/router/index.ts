360402de9df8880180c6f76334be12442bdcf7a7
Allison Robbins <allisonrobbins@gwu.edu>
2024-05-31 14:58:22 -0400

    TAT-141 dynamic page titles (#17)
    * feat(TAT-141): set dynamic page titles based on route
    
    * feat(TAT-141): reference meta.title property to display page name instead of name property
    
    * feat(TAT-141): use nextTick for dynamic page title code
    
    ---------
    
    Co-authored-by: arobbins <arobbins@mitre.org>

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,36 +5,42 @@ import TopTen from "@/views/TopTen.vue";
 import HelpPage from "@/views/HelpPage.vue";
 import TopTenResults from "../views/TopTenResults.vue";
 import CalculatorPage from "../views/CalculatorPage.vue";
+import { nextTick } from "vue";
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "",
     component: HomePage,
   },
   {
     path: "/methodology",
     name: "methodology",
     component: MethodologyPage,
+    meta: { title: "Methodology" },
   },
   {
     path: "/help",
     name: "help",
     component: HelpPage,
+    meta: { title: "Help" },
   },
   {
     path: "/calculator",
     name: "calculator",
     component: CalculatorPage,
+    meta: { title: "Calculator" },
   },
   {
     path: "/top-10-lists",
-    name: "top-10-lists",
+    name: "top-ten-lists",
     component: TopTen,
+    meta: { title: "Top 10 Lists" },
   },
   {
     path: "/calculator/results",
     name: "calculator-results",
     component: TopTenResults,
+    meta: { title: "Calculator Results" },
   },
 ];
 
@@ -45,6 +51,14 @@ const router = createRouter({
     // always scroll to top when linking to a new page
     return { top: 0 };
   },
+});
+
+router.afterEach((to) => {
+  nextTick(() => {
+    document.title = to.meta.title
+      ? to.meta.title + " | Top ATT&CK Techniques"
+      : "Top ATT&CK Techniques";
+  });
 });
 
 export { router };
