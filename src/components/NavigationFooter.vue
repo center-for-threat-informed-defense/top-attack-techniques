26ec2a559ecdd034b52b7077b805163411bbea6f
arobbins <arobbins@mitre.org>
2024-07-02 14:08:31 -0400

    style(TAT-142): set max width for footer

--- src/components/NavigationFooter.vue
+++ src/components/NavigationFooter.vue
@@ -1,48 +1,52 @@
 <template>
-    <div class=" bg-ctid-navy text-white p-16 xl:py-20 xl:px-28 xl:flex">
-        <div class=" xl:w-1/2 xl:pr-10">
-            <h1>
-                <router-link to="/">
-                    <img v-bind:src="Logo" class="h-6" alt="Top Attack Techniques" />
-                </router-link>
-            </h1>
-            <p class="my-4">The <a href="https://mitre-engenuity.org/cybersecurity/center-for-threat-informed-defense/"
-                    target="_blank" class="underline hover:text-ctid-light-purple">Center for Threat-Informed
-                    Defense</a> is a non-profit, privately funded research and development organization. Our mission is
-                to advance the state of the art and the state of the practice in threat-informed defense globally.
-            </p>
-            <div class="md:flex">
-                <img src="../assets/ctid-logo-horizontal-white.png" class="h-6 my-auto"
-                    alt="Center for Threat Informed Defense" />
-                <div class="social-links">
-                    <a href="mailto:ctid@mitre-engenuity.org?subject=Top%20ATT&amp;CK%20Techniques"
-                        aria-label="email ctid" class="email">
-                        <i class="pi pi-envelope"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/showcase/center-for-threat-informed-defense/" target="blank"
-                        aria-label="ctid on linkedin" class="linkedin">
-                        <i class="pi pi-linkedin"></i>
-                    </a>
-                    <a href="https://www.youtube.com/@mitreengenuity" target="blank" class="youtube"
-                        aria-label="mitre engenuity youtube">
-                        <i class="pi pi-youtube"></i>
-                    </a>
-                    <a href="https://github.com/center-for-threat-informed-defense/mappings-explorer" target="blank"
-                        class="github" aria-label="ctid github">
-                        <i class="pi pi-github"></i>
-                    </a>
+    <div class="bg-ctid-navy text-white">
+        <div class="xl:max-w-[1536px] mx-auto p-16 xl:py-20 lg:px-32 2xl:px-16 xl:flex">
+            <div class=" xl:w-1/2 xl:pr-10">
+                <h1>
+                    <router-link to="/">
+                        <img v-bind:src="Logo" class="h-6" alt="Top Attack Techniques" />
+                    </router-link>
+                </h1>
+                <p class="my-4">
+                    The <a href="https://mitre-engenuity.org/cybersecurity/center-for-threat-informed-defense/"
+                        target="_blank" class="underline hover:text-ctid-light-purple">Center for Threat-Informed
+                        Defense</a> is a non-profit, privately funded research and development organization. Our
+                    mission is to advance the state of the art and the state of the practice in threat-informed defense
+                    globally.
+                </p>
+                <div class="md:flex">
+                    <img src="../assets/ctid-logo-horizontal-white.png" class="h-6 my-auto"
+                        alt="Center for Threat Informed Defense" />
+                    <div class="social-links">
+                        <a href="mailto:ctid@mitre-engenuity.org?subject=Top%20ATT&amp;CK%20Techniques"
+                            aria-label="email ctid" class="email">
+                            <i class="pi pi-envelope"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/showcase/center-for-threat-informed-defense/" target="blank"
+                            aria-label="ctid on linkedin" class="linkedin">
+                            <i class="pi pi-linkedin"></i>
+                        </a>
+                        <a href="https://www.youtube.com/@mitreengenuity" target="blank" class="youtube"
+                            aria-label="mitre engenuity youtube">
+                            <i class="pi pi-youtube"></i>
+                        </a>
+                        <a href="https://github.com/center-for-threat-informed-defense/mappings-explorer" target="blank"
+                            class="github" aria-label="ctid github">
+                            <i class="pi pi-github"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="xl:w-1/2 xl:pl-10 xl:my-auto mt-8">
-            <h2 class="mt-8">Related Projects</h2>
-            <div class="internal-links">
-                <div v-for="(item, i) of externalProjects" :key="item.label" class="flex">
-                    <a :href="item.route" target="blank"
-                        class="hover:underline hover:text-ctid-light-purple md:mx-0 mx-2 my-auto">{{
-                        item.label }}</a>
-                    <div v-if="i !== externalProjects.length - 1"
-                        class="md:block hidden mx-4 2xl:mx-8 w-[3px] h-full bg-ctid-light-purple"></div>
+            <div class="xl:max-w-1/2 xl:pl-10 xl:my-auto mt-8">
+                <h2 class="mt-8">Related Projects</h2>
+                <div class="internal-links">
+                    <div v-for="(item, i) of externalProjects" :key="item.label" class="flex">
+                        <a :href="item.route" target="blank"
+                            class="hover:underline hover:text-ctid-light-purple md:mx-0 mx-2 my-auto">{{
+                            item.label }}</a>
+                        <div v-if="i !== externalProjects.length - 1"
+                            class="md:block hidden mx-4 2xl:mx-8 w-[3px] h-full bg-ctid-light-purple"></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -78,7 +82,7 @@ h2 {
 }
 
 .social-links {
-    @apply flex md:ml-8 md:my-0 my-4 gap-8
+    @apply flex md:ml-8 md:my-0 my-4 gap-6 xl:gap-4 2xl:gap-8
 }
 
 .internal-links {
