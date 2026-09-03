79772a4537e929976f3e237761ba09469f396ef4
Alex McLaughlin <amclaughlin@mitre.org>
2026-09-03 14:31:44 -0400

    Update some packages, resolve vulnerabilities, and install vitest.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,6 @@
-import { fileURLToPath, URL } from "url";
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
@@ -10,5 +10,9 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  test: {
+    environment: "node",
+    include: ["src/**/*.{test,spec}.ts"],
   },
 });
